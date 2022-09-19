bfe2bbfcfba795a54ab4464e40d43fb0983636b2
Filipe <filipeferreira.new@gmail.com>
2022-09-18 23:13:46 -0300

    Adicionei os arquivos do Certificard

---
+++ script.js
@@ -0,0 +1,9 @@
+function mudarTema() {
+  document.body.classList.toggle("light");
+}
+
+// Terminar todo o design que está no figma (disponível abaixo)
+// Adicionar suas redes sociais de forma que quem clique consiga abrí-las
+
+//botao para mudar tema
+//colocar conquistas
